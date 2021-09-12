616f9abdbcb4217a726337047f2dd4a1854d34b7
jw2-lillistone <john2.lillistone@live.uwe.ac.uk>
2021-09-12 10:38:54 +0100

    add files

---
+++ DemoDeliveryLibrary/DigitalProductModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoDeliveryLibrary 
+{
+    public class DigitalProductModel : IProductModel
+    {
+        public string Title { get; set; }
+
+        public bool HasOrderBeenCompleted { get; private set; }
+
+        public int TotalDownloadsLeft { get; private set; }
+
+        public void ShipItem(CustomerModel customer)
+        {
+            if( HasOrderBeenCompleted == false)
+            {
+                Console.WriteLine($"Simulating emailing {Title} to { customer.EmailAddress }");
+                TotalDownloadsLeft -= 1;
+                if (TotalDownloadsLeft < 1)
+                {
+                    HasOrderBeenCompleted = true;
+                    TotalDownloadsLeft = 0;
+                }
+            }
+        }
+    }
+}
---
+++ DemoDeliveryLibrary/PhysicalProductModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DemoDeliveryLibrary
+{
+	public class PhysicalProductModel : IProductModel
+	{
+		public string Title { get; set; }
+		public bool HasOrderBeenCompleted{ get; private set; }
+		public void ShipItem(CustomerModel customer)
+		{ 
+			if(HasOrderBeenCompleted == false)
+            {
+				Console.WriteLine($"Simulating shipping {Title} to { customer.FirstName } in { customer.City }");
+				HasOrderBeenCompleted = true;
+            }
+		}
+	}
+}
+
